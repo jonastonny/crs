4fd3cf3fd4a050cd5655ebc86975458fcb3491c8
lomholdt <jonaslomholdt@gmail.com>
2016-02-03 11:45:57 +0100

    Added login/logout button

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.views.generic import *
 
 
-def index(request):
-    return render(request, 'home/index.html')
+class IndexView(TemplateView):
+    template_name = 'home/index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
