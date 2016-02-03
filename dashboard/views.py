223505ddc4a35844c34c31fe4cbd6210eff5d4b8
Jonas Tonny Nielsen <jtn@MacBook-Pro.local>
2016-02-03 11:15:17 +0100

    Added redirect to login

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def dashboard(request):
-    # if request.user.is_authenticated():
-    #     return HttpResponse("Hello, you are logged in.")
-    # else:
-    #     return HttpResponse("NOT LOGGED IN")
-
-    return render(request, 'dashboard/dashboard.html')
+    if request.user.is_authenticated():
+        return render(request, 'dashboard/dashboard.html')
+    else:
+        # return render(request, 'authentication/login_form.html')
+        return redirect('/authentication/login')
