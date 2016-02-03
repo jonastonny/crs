02ce8a604b6cfb07dda9c21c718932cbc1e29417
Jonas Tonny Nielsen <jtn@MacBook-Pro.local>
2016-02-03 16:38:07 +0100

    Changed error message handling

--- authentication/views.py
+++ authentication/views.py
@@ -1,36 +1,15 @@
 from django.shortcuts import render, redirect
-
-from django.forms import EmailField
-
-from django.utils.translation import ugettext_lazy as _
-
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
-
-class UserCreationForm(UserCreationForm):
-    email = EmailField(label=_("Email address"), required=True, help_text=_("Required."))
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
-
-    def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
+from . import forms
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = forms.CustomUserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
             return redirect("/dashboard/")
     else:
-        form = UserCreationForm()
+        form = forms.CustomUserCreationForm()
     return render(request, "authentication/register.html", {
         'form': form,
     })
