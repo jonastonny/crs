2c8d88970e61570087237ac74f7f3c0fe3547ce7
Jonas Tonny Nielsen <jtn@MacBook-Pro.local>
2016-02-04 16:15:44 +0100

    Added views to vote

--- vote/views.py
+++ vote/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from vote.models import Room, Question
 
-# Create your views here.
+
+class RoomDetailView(generic.DetailView):
+    template_name = 'vote/room_detail.html'
+    model = Room
+
+
+class QuestionDetailView(generic.DetailView):
+    template_name = 'vote/question_detail.html'
+    model = Question
