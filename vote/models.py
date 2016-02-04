ad9c6eeeef427a0bbfd7464230b49a7f460bf547
lomholdt <jonaslomholdt@gmail.com>
2016-02-04 13:17:42 +0100

    Crazy update on vote

--- vote/models.py
+++ vote/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 
 class Room(models.Model):
-    user = models.ForeignKey(User)
-    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User)
+    title = models.CharField(max_length=200)
     date_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
 class Question(models.Model):
@@ -22,8 +22,11 @@ class Question(models.Model):
     def number_of_possible_answers(self):
         return self.answer_set.count()
 
-
-    #def number_of_responses:
+    def total_responses(self):
+        total_sum = 0
+        for answer in self.answer_set.all():
+            total_sum += answer.number_of_responses()
+        return total_sum
 
     def __str__(self):
         return self.question_text
@@ -31,21 +34,24 @@ class Question(models.Model):
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    #choices = models.ManyToManyField(User, through='Choice')
     answer_text = models.CharField(max_length=500)
-    date_time = models.DateField()
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    def number_of_responses(self):
+        return self.response_set.count()
 
     def __str__(self):
         return self.answer_text
 
 
-class Choice(models.Model):
+class Response(models.Model):
     user = models.ForeignKey(User)
     answer = models.ForeignKey(Answer)
-    date_time = models.DateField(auto_now_add=True)
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'answer',)
 
     def __str__(self):
         return "%s @ %s" % (self.user.username, self.answer.answer_text)
-
-
 
