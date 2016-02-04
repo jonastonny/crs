faa95ff84faa34fbc86ecf48db848ac68073495d
lomholdt <jonaslomholdt@gmail.com>
2016-02-04 18:18:19 +0100

    Login after register

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from . import forms
 
@@ -7,6 +8,8 @@ def register(request):
         form = forms.CustomUserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
+            user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+            login(request, user)
             return redirect("/dashboard/")
     else:
         form = forms.CustomUserCreationForm()
--- vote/urls.py
+++ vote/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+urlpatterns = [
+    url(r"")
 
+]
