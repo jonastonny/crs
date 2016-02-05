66122ef6d4a6ba678edd24d6ea9f1b388ce3e245
lomholdt <jonaslomholdt@gmail.com>
2016-02-05 12:03:34 +0100

    Moved some JS to be more general

--- vote/urls.py
+++ vote/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^room/create/$', views.CreateRoomView.as_view(), name='room_create'),
     url(r'^room/(?P<pk>[0-9]+)/$', views.RoomDetailView.as_view(), name='room_detail'),
+    url(r'^room/(?P<room>[0-9]+)/edit/$', views.EditRoomDetailView.as_view(), name='room_edit'),
+    url(r'^room/(?P<room>[0-9]+)/subscribe/$', views.subscribe, name='room_subscribe'),
     url(r'^room/(?P<room>[0-9]+)/question/(?P<pk>[0-9]+)/$', views.QuestionDetailView.as_view(), name='question_detail'),
-    url(r'^room/(?P<room>[0-9]+)/subscribe/$', views.subscribe, name='subscribe')
 ]
