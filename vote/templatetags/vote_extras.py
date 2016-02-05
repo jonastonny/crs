4317ca8784f7b91eedf92ef1e1087f3a3189914d
lomholdt <jonaslomholdt@gmail.com>
2016-02-05 11:32:45 +0100

    Fix

--- vote/templatetags/vote_extras.py
+++ vote/templatetags/vote_extras.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.filter
-def room_is_owned_by_user(room, user_id):
-    return room.id == user_id
+def room_is_owned_by_user(room, user):
+    return int(room.id) == int(user.id)
