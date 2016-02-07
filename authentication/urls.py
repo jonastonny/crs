6395f7b8baee3751a0c63880186f9950025f0c46
jonastonny <jonastonnynielsen@gmail.com>
2016-02-07 15:21:02 +0100

    Small removals and added comments

--- authentication/urls.py
+++ authentication/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name': 'authentication/password_reset_done.html'}, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, {'template_name': 'authentication/password_reset_confirm.html'}, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name': 'authentication/password_reset_complete.html'}, name='password_reset_complete'),
-    # url('^', include('django.contrib.auth.urls')),  # kept in order to see how to do the remaining:
+    # Still need to do:
     # ^password_change/$ [name='password_change']
     # ^password_change/done/$ [name='password_change_done']
 ]
