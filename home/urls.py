fb9f7deeb69a32a9a22fefe24ed1f6912d75bb37
lomholdt <jonaslomholdt@gmail.com>
2016-02-08 21:42:01 +0100

    Added ugly profile page

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^home$', views.IndexView.as_view(), name='index'),
+    url(r'^profile/$', views.profile_detail, name='profile'),
+    url(r'^home/$', views.IndexView.as_view(), name='index'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views import generic
 
 from django.views.generic import *
 
@@ -10,9 +11,12 @@ class IndexView(TemplateView):
     template_name = 'home/index.html'
 
     def get(self, request, *args, **kwargs):
-
         if request.user.is_authenticated():
             return redirect('dashboard')
         return render(request, self.template_name)
 
+
+@login_required
+def profile_detail(request):
+    return render(request, template_name='home/profile_detail.html', context={'user': request.user})
 
