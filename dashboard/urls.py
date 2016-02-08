ec042cf3c14072eb39a09f58503a0c4768d75dbc
lomholdt <jonaslomholdt@gmail.com>
2016-02-08 10:44:01 +0100

    Added simple room search feature

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^search/$', views.searchRoom, name='search_room')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,21 @@
+from django.http import HttpResponse
+from django.views import generic
+
+from vote.models import Room
 from django.shortcuts import render, redirect
 
 
 # Create your views here.
+
+
 def dashboard(request):
     if request.user.is_authenticated():
         return render(request, 'dashboard/dashboard.html')
     else:
         return redirect('/authentication/login')
+
+
+def searchRoom(request):
+    query = request.GET.get('q', '')
+    rooms = Room.objects.filter(title__icontains=query)
+    return render(request, template_name='dashboard/search_detail.html', context={'rooms': rooms, 'query': query})
