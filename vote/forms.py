5c3a0899eeba657f847a0d1981291c0eba08df9d
jonastonny <jonastonnynielsen@gmail.com>
2016-02-09 10:42:49 +0100

    Edit group names

--- vote/forms.py
+++ vote/forms.py
@@ -8,3 +8,10 @@ class VoteRoomForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = ['title']
+
+
+class VoteQuestiongroupForm(forms.ModelForm):
+
+    class Meta:
+        model = Room
+        fields = ['title']
--- vote/urls.py
+++ vote/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     # QUESTION GROUP
     url(r'^room/(?P<room>[0-9]+)/group/create/$', views.CreateQuestionGroupView.as_view(), name='questiongroup_create'),
     url(r'^room/(?P<room>[0-9]+)/group/(?P<pk>[0-9]+)/$', views.QuestionGroupDetailView.as_view(), name='questiongroup_detail'),
-    # url(r'^room/(?P<room>[0-9]+)/group/(?P<questiongroup>[0-9]+)/edit/$', views.questiongroup_edit, name='questiongroup_edit'),
+    url(r'^room/(?P<room>[0-9]+)/group/(?P<questiongroup>[0-9]+)/edit/$', views.questiongroup_edit, name='questiongroup_edit'),
+    url(r'^room/(?P<room>[0-9]+)/group/(?P<questiongroup>[0-9]+)/update/$', views.questiongroup_update, name='questiongroup_update'),
     url(r'^room/(?P<room>[0-9]+)/group/(?P<questiongroup>[0-9]+)/toggle/$', views.questiongroup_toggle, name='questiongroup_toggle'),
     # QUESTION
     url(r'^room/(?P<room>[0-9]+)/group/(?P<questiongroup>[0-9]+)/question/(?P<pk>[0-9]+)/$', views.QuestionDetailView.as_view(), name='question_detail'),
