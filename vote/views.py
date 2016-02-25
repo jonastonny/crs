aa527a27cad693165d651d3a937a1733bfc50d59
jonastonny <jonastonnynielsen@gmail.com>
2016-02-25 17:09:45 +0100

    Answer and correct responses

--- vote/views.py
+++ vote/views.py
@@ -23,7 +23,6 @@ class RoomDetailView(generic.DetailView):
     model = Room
 
 
-
 class QuestionDetailView(generic.DetailView):
     template_name = 'vote/question_detail.html'
     model = Question
